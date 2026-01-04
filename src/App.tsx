af7e179a0e24aac662fe8bafaa98bd504a97f388
NipunaLankara <nipunalankara@gmail.com>
2026-01-04 23:09:36 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Header from './components/Header'
+
+function App() {
+    return (
+        <div>
+            <Header
+                title="Moodlyze"
+                subtitle="Understand your emotions"
+            />
+        </div>
+    )
+}
+
+export default App
